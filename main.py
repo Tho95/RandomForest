aead804293f55a5b26e99a7044808da0a6983a91
Thomas Huebscher <hubscherthomas@yahoo.de>
2021-02-18 14:23:30 +0100

    inserted a new dataset glass.csv to analyse, old dataset was not suitable; created preprocess and dataInfo fucntion

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 #Thomas Hübscher / 11.02.2021
-# program for predicting cervical cancer via decison tree and random forest
+# program for predicting glass types via decison tree and random forest
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import dataInfo
+import preprocess
+
+filePath = 'glass.csv'
+X = pd.read_csv(filePath)
+print(X.head())
+
+dataInfo.general(X)
+dataInfo.missing_value_per_column(X)
+dataInfo.colType(X)
+
+y = X['Type'] #(target we want to predict)
+
+X.drop('Type', axis = 1, inplace=True)
+print(X.head())
+
 
